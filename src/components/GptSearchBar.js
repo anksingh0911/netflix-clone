088b8e248fa08a57c1c6706ce4bd7c4da674a624
ankur singh <anksingh0911@gmail.com>
2024-01-14 03:11:35 +0530

    chore: add movies name in gpt slice

--- src/components/GptSearchBar.js
+++ src/components/GptSearchBar.js
@@ -1,22 +1,38 @@
 import React,{useRef} from 'react';
+import { useDispatch } from 'react-redux';
 import { IoSearch } from "react-icons/io5";
 import openAi from '../utils/openai';
+import { addGptSearchMovies } from '../utils/gptSlice';
+import { api_options } from '../utils/constant';
 
 const GptSearchBar = () => {
+  const dispatch =  useDispatch();
   const searchText = useRef(null);
-  let searchQuery  = "Act as a movie recommendation system and suggest some movies for the query: " + searchText.current.value + "only give me 5 movies, comma separated like the example result given head: Example Result: Gadar, 1920, Evil";
-  async function main() {
-    const getMovieChoices = await openAi.chat.completions.create({
-      messages: [{ role: 'user', content: searchQuery }],
-      model: 'gpt-3.5-turbo-0613',
-    });
-    console.log(getMovieChoices.choices, 'hello')
-  }
+
+  // search movie at TMDB
+  const searchMoviesTMDB = async(movieName)=>{
+    const data = await fetch(`https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=false&language=en-US&page=1`,api_options);
+    const movie = data.json()
+    return movie.result;
+  };
+
   const handleSearchClick = ()=>{
     // make an api call to make an api call to get a movie result
-    main()
-    
+    let searchQuery  = "Act as a movie recommendation system and suggest some movies for the query: " + searchText.current.value + "only give me 5 movies, comma separated like the example result given head: Example Result: Gadar, 1920, Evil";
+    async function main() {
+      const getMovieChoices = await openAi.chat.completions.create({
+        messages: [{ role: 'user', content: searchQuery }],
+        model: 'gpt-3.5-turbo-0613',
+      });
+      console.log(getMovieChoices.choices, 'hello')
+      const getMoviesSuggestionList = getMovieChoices.choices?.[0]?.message?.content.split(",");
+      const promiseArray = getMoviesSuggestionList.map((item)=> searchMoviesTMDB(item));
+      const results = await Promise.all(promiseArray);
+      dispatch(addGptSearchMovies({movieNames: getMoviesSuggestionList, movieResults:results}));
+     }
+      main()
   }
+
   return (
     <div className='flex justify-center relative top-4 z-10'>
       <form className='p-2 flex items-center w-[50%]' onSubmit={(e)=> e.preventDefault()}>
@@ -31,4 +47,4 @@ const GptSearchBar = () => {
   )
 }
 
-export default GptSearchBar
+export default GptSearchBar;
--- src/utils/gptSlice.js
+++ src/utils/gptSlice.js
@@ -4,13 +4,20 @@ const gptSlice = createSlice({
   name: "gpt",
   initialState:{
     showGptSearch: false,
+    searchedMovies: null,
+    moviesNames: null
   },
 
   reducers:{
     gptToggleSearchView:(state, action)=>{
       state.showGptSearch = !state.showGptSearch
     },
+    addGptSearchMovies:(state, action)=>{
+      const {movieNames, movieResults} = action.payload;
+      state.moviesNames = movieNames;
+      state.searchedMovies = movieResults;
+    }
   },
 });
-export const {gptToggleSearchView} = gptSlice.actions;
+export const {gptToggleSearchView, addGptSearchMovies} = gptSlice.actions;
 export default gptSlice.reducer;
